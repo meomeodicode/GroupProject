1624f3884ae3960bf17ce048d92228e3d463d656
meomeodicode <128510157+meomeodicode@users.noreply.github.com>
2023-03-26 21:13:14 +0700

    Add files via upload

---
+++ deleteCourse.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "deleteCourse.h"
+using namespace std;
+
+void deleteCourse(Course*& head, int courseId)
+{
+    if (head == nullptr)
+        return;
+    else
+    {
+        Course* cur = head;
+        while (cur)
+        {
+            if (cur->id == courseId)
+            {
+                if (cur == head)
+                {
+                    head = cur->pNext;
+                }
+                else
+                {
+                    cur->pPrev->pNext = cur->pNext;
+                }
+                if
+                    (cur->pNext != nullptr)
+                {
+                    cur->pNext->pPrev = cur->pPrev;
+                }
+                delete cur;
+            }
+            cur = cur->pNext;
+        }
+    }
+}
---
+++ deleteCourse.h
@@ -0,0 +1,7 @@
+#ifndef deleteCourse_H_INCLUDED
+#define deleteCourse_H_INCLUDED
+#include "struct.h"
+using namespace std;
+
+void deleteCourse(Course*& head, int courseId);
+#endif
