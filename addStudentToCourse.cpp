ee0fd84697a4465058dbf79dca1c656af46ea66a
sonahtnk <61734746+sonahtnk@users.noreply.github.com>
2023-03-26 15:51:45 +0700

    Update addStudentToCourse.cpp

--- addStudentToCourse.cpp
+++ addStudentToCourse.cpp
@@ -1,6 +1,40 @@
-void addStudentToCourse(Course *&course, Student student)
+void addStudentToCourse(Student*& s, Course*& c)
 {
-	course->students->pNext=student;
-	course->students=course->students->pNext;
-	course->students->pNext=nullptr;
+    if(c->students==nullptr)
+    {
+        Student* t;
+        c->students=new Student;
+        t=c->students;
+        t->courseEnrol=s->courseEnrol;
+        t->courseTotal=s->courseTotal;
+        t->dateOfBirth=s->dateOfBirth;
+        t->firstName=s->firstName;
+        t->gender=s->gender;
+        t->id=s->id;
+        t->lastName=s->lastName;
+        t->overallGPA=s->overallGPA;
+        t->pNext=nullptr;
+        t->pPrev=nullptr;
+    }
+    else
+    {
+        Student* tmp0=c->students;
+        while(tmp0->pNext!=nullptr)
+        {
+            tmp0=tmp0->pNext;
+        }
+
+        tmp0->pNext=new Student;
+        Student* tmp=tmp0->pNext;
+        tmp->pNext=nullptr;
+        tmp->pPrev=tmp0;
+        tmp->courseEnrol=s->courseEnrol;
+        tmp->courseTotal=s->courseTotal;
+        tmp->dateOfBirth=s->dateOfBirth;
+        tmp->firstName=s->firstName;
+        tmp->gender=s->gender;
+        tmp->id=s->id;
+        tmp->lastName=s->lastName;
+        tmp->overallGPA=s->overallGPA;
+    }
 }
