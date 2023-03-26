6a56a6a3b8c1aa7575708a7a3867ff8b1a75c32d
meomeodicode <128510157+meomeodicode@users.noreply.github.com>
2023-03-26 21:35:01 +0700

    Update importCSV.cpp

--- importCSV.cpp
+++ importCSV.cpp
@@ -10,21 +10,25 @@ void importData()
     ifstream in("scoreData.csv");
     string line;
     const int maxCol = 7;
-    const int maxRow = 200;
-    string studentData[200][7];
+    int row;
+    cin >> row;
+    string* studentData = new string[maxCol * row];
     if (in.is_open())
     {
-    int row = 0;
-    while (getline(in, line) && (row <= maxRow)) 
+    int temp_row = 0;
+    while (getline(in, line) && (temp_row <= row)) 
     {
         stringstream ss(line);
         string info;
         int temp = 0;
         while (getline(ss, info, ',')) 
         {
-            studentData[row][temp++] = info ;
+            studentData[temp_row][temp++] = info ;
         }
-        row++;
+        temp_row++;
     }
+    for (int i = 0; i < row; i++) {
+        delete[] studentData[i];
+    delete[] studentData;
     in.close();
 }
