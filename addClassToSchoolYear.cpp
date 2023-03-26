84f3b307a833befb01009de367c7bc6857128aa0
Vinhapcs <128242214+Vinhapcs@users.noreply.github.com>
2023-03-26 21:57:13 +0700

    sua addclasstoschoolyear

--- addClassToSchoolYear.cpp
+++ addClassToSchoolYear.cpp
@@ -1,11 +1,7 @@
 #include "addClassToSchoolYear.h"
-void addClassToSchoolYear(SchoolYear* &s,string classname,Student* students)
+void addClassToSchoolYear(SchoolYear* &s,Class* newclass)
 {
-    Class* newclass=new Class;
-    newclass->className=classname;
-    newclass->students=students;
-    newclass->pNext=nullptr;
-    newclass->pPrev=nullptr;
+
     if (!s->c)
     {
         s->c=newclass;
--- addClassToSchoolYear.h
+++ addClassToSchoolYear.h
@@ -1,6 +1,6 @@
 #ifndef ADDCLASSTOSCHOOLYEAR_H_INCLUDED
 #define ADDCLASSTOSCHOOLYEAR_H_INCLUDED
 #include "struct.h"
-void addClassToSchoolYear(SchoolYear* &s,string classname,Student* students);
+void addClassToSchoolYear(SchoolYear* &s,Class* newclass);
 
 #endif // ADDCLASSTOSCHOOLYEAR_H_INCLUDED
