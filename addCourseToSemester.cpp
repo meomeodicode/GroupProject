baae34d6e238e3428f1b950ae51e915e54d18f12
kaitouuuu <128288116+kaitouuuu@users.noreply.github.com>
2023-03-27 17:16:17 +0700

    Update addCourseToSemester.cpp

--- addCourseToSemester.cpp
+++ addCourseToSemester.cpp
@@ -1,7 +1,7 @@
 #include "addCourseToSemester.h"
 #include "addCourse.h"
 
-void addCourseToSemester (Semester *&Sem, string id, string courseName, string teacherName, string dayOfWeek, string Session)
+void addCourseToSemester (Semester* &Sem, string id, string courseName, string teacherName, string dayOfWeek, string Session)
 {
     addCourse (Sem->courses, id, courseName, teacherName, dayOfWeek, Session);
 }
