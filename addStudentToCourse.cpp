3d296a73109ed2f2fd431cd9766760ab30b91c50
kaitouuuu <128288116+kaitouuuu@users.noreply.github.com>
2023-03-27 11:48:25 +0700

    Update addStudentToCourse.cpp

--- addStudentToCourse.cpp
+++ addStudentToCourse.cpp
@@ -1,41 +1,28 @@
 #include "addStudentToCourse.h"
-void addStudentToCourse(Student*& s, Course*& c)
-{
-    if(c->students==nullptr)
-    {
-        Student* t;
-        c->students=new Student;
-        t=c->students;
-        t->courseEnrol=s->courseEnrol;
-        t->courseTotal=s->courseTotal;
-        t->dateOfBirth=s->dateOfBirth;
-        t->firstName=s->firstName;
-        t->gender=s->gender;
-        t->id=s->id;
-        t->lastName=s->lastName;
-        t->overallGPA=s->overallGPA;
-        t->pNext=nullptr;
-        t->pPrev=nullptr;
-    }
-    else
-    {
-        Student* tmp0=c->students;
-        while(tmp0->pNext!=nullptr)
-        {
-            tmp0=tmp0->pNext;
-        }
 
-        tmp0->pNext=new Student;
-        Student* tmp=tmp0->pNext;
-        tmp->pNext=nullptr;
-        tmp->pPrev=tmp0;
-        tmp->courseEnrol=s->courseEnrol;
-        tmp->courseTotal=s->courseTotal;
-        tmp->dateOfBirth=s->dateOfBirth;
-        tmp->firstName=s->firstName;
-        tmp->gender=s->gender;
-        tmp->id=s->id;
-        tmp->lastName=s->lastName;
-        tmp->overallGPA=s->overallGPA;
+void addStudentToCourse(Course* &course, string id, string firstName, string lastName, string gender, Date dob, string socialID)
+{
+    // Create a new Student instance
+    Student *newStudent = new Student;
+
+    // Assign values to its properties
+    newStudent->id = id;
+    newStudent->firstName = firstName;
+    newStudent->lastName = lastName;
+    newStudent->gender = gender;
+    newStudent->dateOfBirth = dob;
+    newStudent->socialID = socialID;
+
+    // Add the new student to the beginning of the students list
+    if (course->students == nullptr) {
+        course->students = newStudent;
+        course->students->pNext = nullptr;
+        course->students->pPrev = nullptr;
+    }
+    else {
+        course->students->pPrev = newStudent;
+        newStudent->pNext = course->students;
+        newStudent->pPrev = nullptr;
+        course->students = newStudent;
     }
 }
