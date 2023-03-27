97a8af236c845b63f91138fc0983685ce6e25119
kaitouuuu <128288116+kaitouuuu@users.noreply.github.com>
2023-03-27 17:15:57 +0700

    Update addCourseToSemester.h

--- addCourseToSemester.h
+++ addCourseToSemester.h
@@ -1,4 +1,4 @@
 #pragma once
 #include "struct.h"
 
-void addCourseToSemester (Semester *&Sem, string id, string courseName, string teacherName, string dayOfWeek, string Session);
+void addCourseToSemester (Semester* &Sem, string id, string courseName, string teacherName, string dayOfWeek, string Session);
