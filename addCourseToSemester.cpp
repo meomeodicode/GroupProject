537581d66408ff03fdbac1fedbc74767f3301f62
kaitouuuu <128288116+kaitouuuu@users.noreply.github.com>
2023-03-27 07:30:11 +0700

    Update addCourseToSemester.cpp

--- addCourseToSemester.cpp
+++ addCourseToSemester.cpp
@@ -1,24 +1,6 @@
 #include "addCourseToSemester.h"
 #include "addCourse.h"
-void addCourseToSemester(Semester *&Sem, Course *&cou,string id,string courseName,string teacherName,string dayOfWeek,string Session)
+void addCourseToSemester(Semester *&Sem, string id,string courseName,string teacherName,string dayOfWeek,string Session)
 {
-    addCourse(cou, id, courseName, teacherName, dayOfWeek, Session);
-    if(Sem->courses==nullptr)
-    {
-        Sem->courses=new Course;
-        Course* tmp=Sem->courses;
-        tmp->pNext=nullptr;
-        tmp->pPrev=nullptr;
-        tmp=cou;
-    }
-    else
-    {
-        Course* tmp0=Sem->courses;
-        while(tmp0->pNext!=nullptr) tmp0=tmp0->pNext;
-        tmp0->pNext=new Course;
-        Course* tmp=tmp0->pNext;
-        tmp=cou;
-        tmp->pNext=nullptr;
-        tmp->pPrev=tmp0;
-    }
+    addCourse(Sem->courses, id, courseName, teacherName, dayOfWeek, Session);
 }
