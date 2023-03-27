81b5a0b185adb35b6c40f2d5955454ef29228a17
kaitouuuu <128288116+kaitouuuu@users.noreply.github.com>
2023-03-27 12:17:32 +0700

    Update updateResult.cpp

--- updateResult.cpp
+++ updateResult.cpp
@@ -2,39 +2,36 @@
 #include "updateResult.h"
 #include <fstream>
 #include <string>
+
 using namespace std;
 
-void updateResult(Course* &head, int id, int studentID)
+void updateResult(Course* &head, string id, string studentID)
 {
     Course* cur = head;
     double input;
     int count = 1;
      if (cur->id == id)
      {
-            if (cur->students->id == studentID)
+        if (cur->students->id == studentID)
+        {
+            while ((cin >> input) && (count <= 3))
             {
-                    while ((cin>>input)&&(count<=3))
-                    {
-                        if (count == 1) 
-                        {
-                            cur->otherMark = input;
-                            ++count;
-                        }
-                        else if (count == 2) 
-                        {
-                            cur->midtermMark = input;
-                            ++count;
-                        }
-                        else if (count == 3)
-                        {
-                            cur->finalMark = input;
-                            ++count;
-                        }
-
-                    }
+                if (count == 1) 
+                {
+                    cur->otherMark = input;
+                    ++count;
+                }
+                else if (count == 2) 
+                {
+                    cur->midtermMark = input;
+                    ++count;
+                }
+                else if (count == 3)
+                {
+                    cur->finalMark = input;
+                    ++count;
+                }
             }
-        
+        }
     }
 }
-
-
