85c9e0db7f2ab23f460b3f8ac1a27ffbaefd5da0
kaitouuuu <128288116+kaitouuuu@users.noreply.github.com>
2023-03-27 11:59:33 +0700

    Update deleteCourse.h

--- deleteCourse.h
+++ deleteCourse.h
@@ -1,7 +1,5 @@
-#ifndef deleteCourse_H_INCLUDED
-#define deleteCourse_H_INCLUDED
+#pragma once
 #include "struct.h"
 using namespace std;
 
-void deleteCourse(Course*& head, int courseId);
-#endif
+void deleteCourse(Course* &head, string courseId);
