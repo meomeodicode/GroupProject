e66bdc47124a510175563394616a6ee0124a65ce
kaitouuuu <128288116+kaitouuuu@users.noreply.github.com>
2023-03-27 11:53:48 +0700

    Update addClassToSchoolYear.h

--- addClassToSchoolYear.h
+++ addClassToSchoolYear.h
@@ -1,6 +1,6 @@
 #ifndef ADDCLASSTOSCHOOLYEAR_H_INCLUDED
 #define ADDCLASSTOSCHOOLYEAR_H_INCLUDED
 #include "struct.h"
-void addClassToSchoolYear(SchoolYear* &s,string classname);
+void addClassToSchoolYear (SchoolYear* &s, string classname);
 
 #endif // ADDCLASSTOSCHOOLYEAR_H_INCLUDED
