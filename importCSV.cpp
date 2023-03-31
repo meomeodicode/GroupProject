f381b894fd803b415b24a79035b92bfebcd90be3
meomeodicode <128510157+meomeodicode@users.noreply.github.com>
2023-03-31 17:18:54 +0700

    Update importCSV.cpp

--- importCSV.cpp
+++ importCSV.cpp
@@ -1,5 +1,4 @@
 #include "struct.h"
-
 using namespace std;
 
 void importData()
@@ -9,23 +8,28 @@ void importData()
     const int maxCol = 7;
     int row;
     cin >> row;
-    string* studentData = new string[maxCol * row];
+    string** studentData = new string*[row];
+    for (int i = 0; i < row; i++) {
+        studentData[i] = new string[maxCol];
+    }
     if (in.is_open())
     {
-    int temp_row = 0;
-    while (getline(in, line) && (temp_row <= row)) 
-    {
-        stringstream ss(line);
-        string info;
-        int temp = 0;
-        while (getline(ss, info, ',')) 
+        int temp_row = 0;
+        while (getline(in, line) && (temp_row < row)) 
         {
-            studentData[temp_row][temp++] = info ;
+            stringstream ss(line);
+            string info;
+            int column = 0;
+            while (getline(ss, info, ',')) 
+            {
+                studentData[temp_row][column++] = info;
+            }
+            temp_row++;
         }
-        temp_row++;
+        in.close();
     }
     for (int i = 0; i < row; i++) {
         delete[] studentData[i];
+    }
     delete[] studentData;
-    in.close();
 }
