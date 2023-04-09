7e8da55aa68e4730cef9e65b28fe20456a0142b9
sonahtnk <61734746+sonahtnk@users.noreply.github.com>
2023-04-09 22:46:43 +0700

    Update viewStudentScoreBoard.cpp

--- viewStudentScoreBoard.cpp
+++ viewStudentScoreBoard.cpp
@@ -8,14 +8,15 @@ ViewStudentScoreboard(Student S)
     cout << S.firstName << endl;
     cout << S.lastName << endl;
     cout << S.gender << endl;
-    cout << “Overall GPA : “ << S.overallGPA << endl;
-    cout << cur->firstName << endl;
+    cout << "Overall GPA : " << S.overallGPA << endl;
     Course* curcourse = S.courseTotalHead;
+    double coursenumber=0; double totalpoint=0;
     while (curcourse)
     {
         coursenumber += 1;
         totalpoint += curcourse->courseGPA;
-        cout << curcourse->courseName << “ “ << curcourse->credits << “ “ << curcourse->midtermMark << “ “ << curcourse->otherMark << “ “ << curcourse->finalMark << “ “ << curcourse->totalMark << “ “ << curcourse->courseGPA << endl;
-        curcouse = curcourse->pNext;
+        cout << curcourse->courseName << " " << curcourse->credits << " " << curcourse->midtermMark << " " << curcourse->otherMark << " " << curcourse->finalMark << " " << curcourse->totalMark << " " << curcourse->courseGPA << endl;
+        curcourse = curcourse->pNext;
     }
+    cout<<"Semester GPA: "<<totalpoint/coursenumber<<endl;
 }
